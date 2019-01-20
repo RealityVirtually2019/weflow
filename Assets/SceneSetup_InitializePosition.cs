cf1fd6b7c137d62d91433797986b1f6d63a981ca
Alexander Gao <awg297@nyu.edu>
2019-01-19 20:02:16 -0500

    8pm Commit

---
+++ Assets/SceneSetup_InitializePosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class SceneSetup_InitializePosition : MonoBehaviour
+{
+
+    public Camera MLcam;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = MLcam.transform.forward * 0.5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
---
+++ Assets/TriggerEvent_01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class TriggerEvent_01 : MonoBehaviour
+{
+
+    public Animator anim;
+    public GameObject B_Target_R_JustDoIt;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("Target_TransitionOn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play("Target_TransitionOff");
+        //this.SetActive(false);
+        B_Target_R_JustDoIt.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
